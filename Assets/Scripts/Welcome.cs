474199099ffd160aef90f268296f7b098362d7ac
ywt <yaowt19@mails.tsinghua.edu.cn>
2020-10-07 01:05:16 +0800

    Part1 OpenFile

--- Assets/Scripts/Welcome.cs
+++ Assets/Scripts/Welcome.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Welcome : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void Start()
+    public GameObject openFileDialogPrefab;
+
+    public void OpenFile()
     {
-        
+        var openFileDialog = Instantiate(openFileDialogPrefab);
+        Dialog dialog = openFileDialog.GetComponent<Dialog>();
+
+        // Choose a file
+        dialog.OpenFileDialog("Try Choosing a txt file", @"C:\StartingLocationHere", ".txt", OnDialogComplete);
     }
 
-    // Update is called once per frame
-    private void Update()
+    void OnDialogComplete(bool isSucessful, string path)
     {
-        
+        if (isSucessful)
+        {
+            Debug.Log("Path : " + path);
+        }
+        else
+        {
+            Debug.Log("No File/Folder Chosen, Cancel was pressed or something else happened.");
+        }
     }
 }
