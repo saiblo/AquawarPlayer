3f10622f12b7cff5cd9fad7453a7e9232f93a2b7
UNIDY2002 <UNIDY2002@outlook.com>
2020-10-16 23:48:51 +0800

    Add status bar (#2).

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    public Transform statusBarPrefab;
+
+    public Transform myStatusRoot;
+    public Transform enemyStatusRoot;
+
+    private void Awake()
+    {
+        for (var i = 0; i < 4; i++)
+        {
+            Instantiate(statusBarPrefab, myStatusRoot)
+                .localPosition = new Vector3(10, -15 * i - 10);
+            Instantiate(statusBarPrefab, enemyStatusRoot)
+                .localPosition = new Vector3(10, -15 * i - 10);
+        }
+    }
+}
