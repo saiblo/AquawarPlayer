ff4ff2f4e230e15fa443fe61d57dff0ee5d84730
UNIDY2002 <UNIDY2002@outlook.com>
2020-10-16 20:52:59 +0800

    Generate eight fish (using spheres to represent fish...) (#2).

---
+++ Assets/Scripts/Game/AllFish.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class AllFish : MonoBehaviour
+    {
+        public Transform fishPrefab;
+
+        private void Awake()
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                Instantiate(fishPrefab, transform)
+                    .localPosition = new Vector3(2 * (i + 2), 0, 2 - i);
+                Instantiate(fishPrefab, transform)
+                    .localPosition = new Vector3(-2 * (i + 2), 0, 2 - i);
+            }
+        }
+    }
+}
