e78b84ea2d31f767251ec6d2a2568485194af798
UNIDY2002 <UNIDY2002@outlook.com>
2020-10-17 11:27:07 +0800

    Add a preparation scene (#6).

---
+++ Assets/Scripts/Preparation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Preparation : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    public void ConfirmSelection()
+    {
+        SceneManager.LoadScene("Scenes/Game");
+    }
+}
