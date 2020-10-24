cc63fc1886f6dcf33bf217a8367c4351958e11ab
UNIDY2002 <UNIDY2002@outlook.com>
2020-10-24 23:54:56 +0800

    Show 6 banned fish (#10).

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,5 @@
+﻿public static class Constants
+{
+    public const int BanNum = 6;
+    public const int FishNum = 18;
+}
--- Assets/Scripts/Preparation.cs
+++ Assets/Scripts/Preparation.cs
@@ -10,30 +10,53 @@ public class Preparation : MonoBehaviour
 
     public Transform allFishRoot;
 
+    private readonly MeshRenderer[] _fishMeshRenderers = new MeshRenderer[18];
+
+    private readonly EventTrigger[] _fishEventTriggers = new EventTrigger[18];
+
     private readonly bool[] _fishSelected =
-        {false, false, false, false, false, false, false, false, false, false, false, false};
+    {
+        false, false, false, false, false, false,
+        false, false, false, false, false, false,
+        false, false, false, false, false, false
+    };
 
     public Button doneButton;
 
     private void Awake()
     {
-        for (var i = 0; i < 2; i++)
+        for (var i = 0; i < 3; i++)
         {
             for (var j = 0; j < 6; j++)
             {
                 var fish = Instantiate(fishPrefab, allFishRoot);
                 fish.localPosition = new Vector3(j * 3 - 7.5f, -i * 3);
-                var fishMeshRenderer = fish.GetComponent<MeshRenderer>();
-                var trigger = new EventTrigger.Entry();
                 var id = i * 6 + j;
-                trigger.callback.AddListener(delegate
-                {
-                    _fishSelected[id] = !_fishSelected[id];
-                    fishMeshRenderer.material.color = _fishSelected[id] ? Color.green : Color.gray;
-                });
-                fishMeshRenderer.material.color = Color.gray;
-                fish.GetComponent<EventTrigger>().triggers.Add(trigger);
+                _fishMeshRenderers[id] = fish.GetComponent<MeshRenderer>();
+                _fishMeshRenderers[id].material.color = Color.gray;
+                _fishEventTriggers[id] = fish.GetComponent<EventTrigger>();
             }
+        }
+        for (var i = 0; i < Constants.BanNum; i++)
+        {
+            var id = i;
+            _fishMeshRenderers[id].material.color = Color.red;
+        }
+        ActivateFishTriggers();
+    }
+
+    private void ActivateFishTriggers()
+    {
+        for (var i = Constants.BanNum; i < Constants.FishNum; i++)
+        {
+            var id = i;
+            var trigger = new EventTrigger.Entry();
+            trigger.callback.AddListener(delegate
+            {
+                _fishSelected[id] = !_fishSelected[id];
+                _fishMeshRenderers[id].material.color = _fishSelected[id] ? Color.green : Color.gray;
+            });
+            _fishEventTriggers[id].triggers.Add(trigger);
         }
     }
 
