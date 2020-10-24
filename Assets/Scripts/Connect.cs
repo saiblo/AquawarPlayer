aae50575aa8897ca9187804c05c3e91372b8c550
UNIDY2002 <UNIDY2002@outlook.com>
2020-10-25 03:10:08 +0800

    Implement welcome scene design improvement (#11).

---
+++ Assets/Scripts/Connect.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using LitJson;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Connect : MonoBehaviour
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+    private class ConnectRequest
+    {
+        public string token { get; set; }
+        public string request { get; set; }
+    }
+
+    public async void ConnectRoom(InputField tokenInputField)
+    {
+        var tokenEncoded = tokenInputField.text;
+        var tokenDecoded = Encoding.UTF8.GetString(Convert.FromBase64String(tokenEncoded));
+        if (tokenDecoded == tokenEncoded)
+        {
+            Debug.Log("Token解码失败");
+            return;
+        }
+        try
+        {
+            Debug.Log("连接中……");
+            var ws = new ClientWebSocket();
+            await ws.ConnectAsync(new Uri($"wss://{tokenDecoded}"), CancellationToken.None);
+            var request = new ConnectRequest {token = tokenEncoded, request = "connect"};
+            await ws.SendAsync(
+                new ArraySegment<byte>(Encoding.UTF8.GetBytes(JsonMapper.ToJson(request))),
+                WebSocketMessageType.Text,
+                true,
+                CancellationToken.None
+            );
+            Debug.Log("连接成功");
+            SceneManager.LoadScene("Scenes/Preparation");
+        }
+        catch (Exception)
+        {
+            // ReSharper disable once Unity.InefficientPropertyAccess
+            Debug.Log("连接失败");
+        }
+    }
+}
--- Assets/Scripts/Welcome.cs
+++ Assets/Scripts/Welcome.cs
@@ -56,41 +56,8 @@ public class Welcome : MonoBehaviour
                 });
     }
 
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
-    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
-    private class ConnectRequest
+    public void GoConnect()
     {
-        public string token { get; set; }
-        public string request { get; set; }
-    }
-
-    public async void ConnectRoom(InputField tokenInputField)
-    {
-        var tokenEncoded = tokenInputField.text;
-        var tokenDecoded = Encoding.UTF8.GetString(Convert.FromBase64String(tokenEncoded));
-        if (tokenDecoded == tokenEncoded)
-        {
-            Debug.Log("Token解码失败");
-            return;
-        }
-        try
-        {
-            Debug.Log("连接中……");
-            var ws = new ClientWebSocket();
-            await ws.ConnectAsync(new Uri($"wss://{tokenDecoded}"), CancellationToken.None);
-            var request = new ConnectRequest {token = tokenEncoded, request = "connect"};
-            await ws.SendAsync(
-                new ArraySegment<byte>(Encoding.UTF8.GetBytes(JsonMapper.ToJson(request))),
-                WebSocketMessageType.Text,
-                true,
-                CancellationToken.None
-            );
-            Debug.Log("连接成功");
-        }
-        catch (Exception)
-        {
-            // ReSharper disable once Unity.InefficientPropertyAccess
-            Debug.Log("连接失败");
-        }
+        SceneManager.LoadScene("Scenes/Connect");
     }
 }
