43a369892b2750d5db14257a36e751248f8efc40
UNIDY2002 <UNIDY2002@outlook.com>
2020-11-01 01:09:57 +0800

    Add a naive entrance effect for preparation screen

--- Assets/Scripts/Preparation.cs
+++ Assets/Scripts/Preparation.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class Preparation : MonoBehaviour
 {
@@ -13,7 +15,21 @@ public class Preparation : MonoBehaviour
 
     private readonly Transform[] _fishTransforms = new Transform[Constants.FishNum];
 
+    private readonly Vector3[] _targetPositions = new Vector3[Constants.FishNum];
+
     private readonly EventTrigger[] _fishEventTriggers = new EventTrigger[Constants.FishNum];
+
+    private readonly List<int> _entranceSpeedX = new List<int>();
+
+    private readonly List<int> _entranceSpeedY = new List<int>();
+
+    private readonly Random _random = new Random(Convert.ToInt32(DateTime.Now.Ticks % 100000000));
+
+    private DateTime _initialTime;
+
+    private bool _animationPlayed;
+
+    private const float EntranceDuration = 1200.0f;
 
     private readonly bool[] _fishSelected =
     {
@@ -30,32 +46,22 @@ public class Preparation : MonoBehaviour
 
     private void Awake()
     {
+        for (var i = 0; i < Constants.FishNum; i++)
+        {
+            _entranceSpeedX.Add(_random.Next(-4, 5));
+            _entranceSpeedY.Add(_random.Next(-4, 5));
+        }
         for (var i = 0; i < 3; i++)
         {
             for (var j = 0; j < 6; j++)
             {
                 var id = i * 6 + j;
                 _fishTransforms[id] = Instantiate(PrefabRefs.FishPrefabs[id], allFishRoot);
-                _fishTransforms[id].localPosition = new Vector3(j * 3 - 7.5f, -i * 3);
+                _targetPositions[id] = new Vector3(j * 3 - 7.5f, -i * 3);
                 _fishEventTriggers[id] = _fishTransforms[id].GetComponent<EventTrigger>();
             }
         }
-        for (var i = 0; i < Constants.BanNum; i++)
-        {
-            var id = i;
-            _timers[id] = new Timer(
-                state =>
-                {
-                    _uiQueue.Enqueue(() =>
-                    {
-                        _fishTransforms[id].localScale = new Vector3(2, 2, 2);
-                        if (id != 5) return;
-                        foreach (var timer in _timers) timer.Dispose();
-                        ActivateFishTriggers();
-                    });
-                },
-                null, i * 500, 0);
-        }
+        _initialTime = DateTime.Now;
     }
 
     private void ActivateFishTriggers()
@@ -80,8 +86,45 @@ public class Preparation : MonoBehaviour
 
     private void Update()
     {
-        doneButton.interactable = _fishSelected.Count(b => b) == 4;
-        while (_uiQueue.Count > 0)
-            _uiQueue.Dequeue()();
+        if (!_animationPlayed)
+        {
+            var timeDiff =
+                float.Parse((DateTime.Now - _initialTime).TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+            for (var i = 0; i < Constants.FishNum; i++)
+            {
+                _fishTransforms[i].localPosition =
+                    _targetPositions[i] - new Vector3(
+                        Math.Max(0, EntranceDuration - timeDiff) / 200f * (_entranceSpeedX[i] - 0.5f),
+                        Math.Max(0, EntranceDuration - timeDiff) / 200f * (_entranceSpeedY[i] - 0.5f)
+                    );
+            }
+            // ReSharper disable once InvertIf
+            if (timeDiff > EntranceDuration)
+            {
+                _animationPlayed = true;
+                for (var i = 0; i < Constants.BanNum; i++)
+                {
+                    var id = i;
+                    _timers[id] = new Timer(
+                        state =>
+                        {
+                            _uiQueue.Enqueue(() =>
+                            {
+                                _fishTransforms[id].localScale = new Vector3(2, 2, 2);
+                                if (id != 5) return;
+                                foreach (var timer in _timers) timer.Dispose();
+                                ActivateFishTriggers();
+                            });
+                        },
+                        null, i * 500, 0);
+                }
+            }
+        }
+        else
+        {
+            doneButton.interactable = _fishSelected.Count(b => b) == 4;
+            while (_uiQueue.Count > 0)
+                _uiQueue.Dequeue()();
+        }
     }
 }
