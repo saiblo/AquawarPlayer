ba69049eede660c0e2b51b4e2073fcfa65aa1279
UNIDY2002 <UNIDY2002@outlook.com>
2020-11-01 00:13:41 +0800

    Add a naive "animation" to welcome screen

--- Assets/Scripts/Welcome.cs
+++ Assets/Scripts/Welcome.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,10 +10,23 @@ public class Welcome : MonoBehaviour
 
     public Transform fishPrefabSample;
 
+    public Transform bubbleOpenFile;
+
+    public Transform bubbleConnect;
+
+    private Vector3 _bubbleOpenFileOriginalPos;
+
+    private Vector3 _bubbleConnectOriginalPos;
+
+    private DateTime _initialTime;
+
     private void Awake()
     {
         for (var i = 0; i < Constants.FishNum; i++)
             PrefabRefs.FishPrefabs[i] = fishPrefabSample;
+        _bubbleOpenFileOriginalPos = bubbleOpenFile.localPosition;
+        _bubbleConnectOriginalPos = bubbleConnect.localPosition;
+        _initialTime = DateTime.Now;
     }
 
     public void OpenFile()
@@ -63,5 +77,27 @@ public class Welcome : MonoBehaviour
     public void BubbleOnLoseHover(Transform bubble)
     {
         bubble.localScale = new Vector3(5, 5, 5);
+    }
+
+    private static float GetNewPos(float original, TimeSpan timeSpan, int omega, float a)
+    {
+        return float.Parse(
+            (original + a * Math.Sin(timeSpan.TotalMilliseconds / Convert.ToDouble(omega))).ToString(
+                CultureInfo.InvariantCulture));
+    }
+
+    private void Update()
+    {
+        var timeSpan = DateTime.Now - _initialTime;
+        bubbleOpenFile.localPosition = new Vector3(
+            GetNewPos(_bubbleOpenFileOriginalPos.x, timeSpan, 1500, 0.8f),
+            GetNewPos(_bubbleOpenFileOriginalPos.y, timeSpan, 2048, 0.6f),
+            GetNewPos(_bubbleOpenFileOriginalPos.z, timeSpan, 320, 0.1f)
+        );
+        bubbleConnect.localPosition = new Vector3(
+            GetNewPos(_bubbleConnectOriginalPos.x, timeSpan, 1280, -0.8f),
+            GetNewPos(_bubbleConnectOriginalPos.y, timeSpan, 2333, 0.6f),
+            GetNewPos(_bubbleConnectOriginalPos.z, timeSpan, 361, 0.1f)
+        );
     }
 }
