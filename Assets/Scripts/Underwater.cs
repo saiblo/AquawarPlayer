1215dcc9a47215a3e01259da8929b6226f985578
UNIDY2002 <UNIDY2002@outlook.com>
2020-12-13 00:44:20 +0800

    F**k it background! HOW TO IMPLEMENT? I DO NOT KNOW!

---
+++ Assets/Scripts/Underwater.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Underwater : MonoBehaviour
+{
+    private void Awake()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = new Color(0.1f, 0.13f, 0.36f, 0.8f);
+        RenderSettings.fogDensity = 0.01f;
+    }
+}
