8d83ef4f8ae6a6c1c0fd4216bc77e71c83cd9ae3
UNIDY2002 <UNIDY2002@outlook.com>
2021-01-12 08:23:51 +0800

    Code cleanup

--- Assets/Scripts/Welcome.cs
+++ Assets/Scripts/Welcome.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Globalization;
 using System.IO;
 using System.Text;
 using LitJson;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Utils;
-using Random = System.Random;
 
 public class Welcome : MonoBehaviour
 {
@@ -14,36 +12,11 @@ public class Welcome : MonoBehaviour
 
     public Transform[] fishPrefabSamples;
 
-    public Transform bubbleOpenFile;
-
-    public Transform bubbleConnect;
-
-    public Transform wanderingBubble;
-
-    private Vector3 _bubbleOpenFileOriginalPos;
-
-    private Vector3 _bubbleConnectOriginalPos;
-
-    private DateTime _initialTime;
-
-    private int _wanderingBubbleRound = -1;
-
-    private const int WanderingBubbleInterval = 15000;
-
-    private readonly Random _random = new Random(Convert.ToInt32(DateTime.Now.Ticks % 100000000));
-
-    private float _currRandX;
-
-    private double _timeBias;
-
     private void Awake()
     {
         Screen.SetResolution(1920, 1080, true);
         for (var i = 0; i < Constants.FishNum; i++)
             SharedRefs.FishPrefabs[i] = fishPrefabSamples[i];
-        _bubbleOpenFileOriginalPos = bubbleOpenFile.localPosition;
-        _bubbleConnectOriginalPos = bubbleConnect.localPosition;
-        _initialTime = DateTime.Now;
     }
 
     public void OpenFile()
@@ -94,67 +67,5 @@ public class Welcome : MonoBehaviour
     public void GoConnect()
     {
         SceneManager.LoadScene("Scenes/Connect");
-    }
-
-    public void BubbleOnHover(Transform bubble)
-    {
-        bubble.localScale = new Vector3(6, 6, 6);
-    }
-
-    public void BubbleOnLoseHover(Transform bubble)
-    {
-        bubble.localScale = new Vector3(5, 5, 5);
-    }
-
-    public void WanderingBubbleOnHover()
-    {
-        wanderingBubble.localScale = new Vector3(3, 3, 3);
-    }
-
-    public void WanderingBubbleOnLoseHover()
-    {
-        wanderingBubble.localScale = new Vector3(2, 2, 2);
-    }
-
-    public void WanderingBubbleOnClick()
-    {
-        wanderingBubble.localScale = new Vector3(2, 2, 2);
-        _wanderingBubbleRound++;
-        _currRandX = _random.Next(-54, 54) / 3.0f;
-        var actualSpan = (DateTime.Now - _initialTime).TotalMilliseconds + _timeBias;
-        if (Math.Abs(Math.Floor(actualSpan / WanderingBubbleInterval) -
-                     Math.Floor((actualSpan + 8000) / WanderingBubbleInterval)) < 0.1)
-            _timeBias += 8000;
-    }
-
-    private static float GetNewPos(float original, TimeSpan timeSpan, int omega, float a)
-    {
-        return float.Parse(
-            (original + a * Math.Sin(timeSpan.TotalMilliseconds / Convert.ToDouble(omega))).ToString(
-                CultureInfo.InvariantCulture));
-    }
-
-    private void Update()
-    {
-        var timeSpan = DateTime.Now - _initialTime;
-        bubbleOpenFile.localPosition = new Vector3(
-            GetNewPos(_bubbleOpenFileOriginalPos.x, timeSpan, 1500, 0.8f),
-            GetNewPos(_bubbleOpenFileOriginalPos.y, timeSpan, 2048, 0.6f),
-            GetNewPos(_bubbleOpenFileOriginalPos.z, timeSpan, 320, 0.1f)
-        );
-        bubbleConnect.localPosition = new Vector3(
-            GetNewPos(_bubbleConnectOriginalPos.x, timeSpan, 1280, -0.8f),
-            GetNewPos(_bubbleConnectOriginalPos.y, timeSpan, 2333, 0.6f),
-            GetNewPos(_bubbleConnectOriginalPos.z, timeSpan, 361, 0.1f)
-        );
-        var actualSpan = timeSpan.TotalMilliseconds + _timeBias;
-        if (Math.Floor(actualSpan / WanderingBubbleInterval) > _wanderingBubbleRound)
-        {
-            _wanderingBubbleRound = Convert.ToInt32(actualSpan / WanderingBubbleInterval);
-            _currRandX = _random.Next(-54, 54) / 3.0f;
-        }
-        var remainder = float.Parse(
-            (actualSpan - _wanderingBubbleRound * WanderingBubbleInterval).ToString(CultureInfo.InvariantCulture));
-        wanderingBubble.localPosition = new Vector3(_currRandX, remainder * 0.002f - 11f, 6);
     }
 }
