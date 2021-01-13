1eaed25a6f0c676436b028d76beb9d2e564ea93a
ywt <yaowt19@mails.tsinghua.edu.cn>
2021-01-13 13:22:39 +0800

    Fish image border

--- Assets/Scripts/Components/GlanceFish.cs
+++ Assets/Scripts/Components/GlanceFish.cs
@@ -9,6 +9,7 @@ namespace Components
     {
         public Image fishAvatar;
         public Image mask;
+        public Image border;
 
         public void SetupFish(int id, Constants.FishState state)
         {
@@ -17,12 +18,15 @@ namespace Components
             {
                 case Constants.FishState.Used:
                     mask.color = new Color(1, 0, 0, 0.3f);
+                    border.color = Color.grey;
                     break;
                 case Constants.FishState.Using:
                     mask.color = new Color(0, 0, 0, 0);
+                    border.color = Color.white;
                     break;
                 case Constants.FishState.Free:
                     mask.color = new Color(0, 0, 0, 0.6f);
+                    border.color = new Color(0, 128, 0, 0.6f);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(state), state, null);
