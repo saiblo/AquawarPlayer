79a056b864cfb16842ab228f123dce2c41477d2e
UNIDY2002 <UNIDY2002@outlook.com>
2021-01-15 07:08:33 +0800

    Allow switching from fullscreen and not fullscreen

---
+++ Assets/Scripts/Components/FullScreen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Components
+{
+    public class FullScreen : MonoBehaviour
+    {
+        private bool _fullScreen = true;
+
+        public Text prompt;
+
+        public void Toggle()
+        {
+            _fullScreen = !_fullScreen;
+            Screen.fullScreen = _fullScreen;
+        }
+
+        private void Awake()
+        {
+            Screen.fullScreen = true;
+        }
+
+        private void Update()
+        {
+            prompt.text = _fullScreen ? "退出全屏" : "进入全屏";
+        }
+    }
+}
