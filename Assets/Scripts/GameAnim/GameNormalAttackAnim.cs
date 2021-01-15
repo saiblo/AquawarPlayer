6c842ef3156618e24cfaa783b1073d42955d835b
UNIDY2002 <UNIDY2002@outlook.com>
2021-01-15 13:15:29 +0800

    Reimplement GameNormalAttackAnim.cs

--- Assets/Scripts/GameAnim/GameNormalAttackAnim.cs
+++ Assets/Scripts/GameAnim/GameNormalAttackAnim.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using GameHelper;
 using LitJson;
 
@@ -8,28 +9,19 @@ namespace GameAnim
     {
         public static void NormalAttackAnim(this GameUI gameUI, JsonData actionInfo)
         {
-            var enemy = !gameUI.GameState.MyTurn;
-            var selected = (int) actionInfo["ActionFish"];
-            var target = 0;
-            for (var i = 0; i < 4; i++)
-            {
-                // ReSharper disable once InvertIf
-                if (enemy && gameUI.GameState.MyFishSelectedAsTarget[i] ||
-                    gameUI.GameState.EnemyFishSelectedAsTarget[i])
-                {
-                    target = i;
-                    break;
-                }
-            }
+            var actionFish = (int) actionInfo["ActionFish"];
+            var target = (int) actionInfo["hit"].OfType<JsonData>().First(e => (bool) e["traceable"])["target"];
+
+            var myTurn = gameUI.GameState.MyTurn;
             var distance =
-                GameObjectManager.FishRelativePosition(enemy, selected) -
-                GameObjectManager.FishRelativePosition(!enemy, target);
+                GameObjectManager.FishRelativePosition(!myTurn, actionFish) -
+                GameObjectManager.FishRelativePosition(myTurn, target);
             gameUI.Repeat(cnt =>
             {
-                (enemy ? gameUI.Gom.EnemyFishTransforms : gameUI.Gom.MyFishTransforms)[selected].localPosition =
-                    GameObjectManager.FishRelativePosition(!enemy, target) + Math.Abs(cnt - 40f) / 40f * distance;
+                (myTurn ? gameUI.Gom.MyFishTransforms : gameUI.Gom.EnemyFishTransforms)[actionFish].localPosition =
+                    GameObjectManager.FishRelativePosition(myTurn, target) +
+                    Math.Abs(cnt - 40f) / 40f * distance;
             }, () => { }, 81, 0, 10);
-            gameUI.GameState.PassiveList.Add(target);
         }
     }
 }
